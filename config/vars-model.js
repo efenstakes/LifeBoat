d69b45f9541f3873963f49ca7daee600142dbcb5
efenstakes <efenstakes101@gmail.com>
2019-03-26 10:14:10 +0300

    add vars-model.js file which models vars.js which should contain application secret variables

---
+++ config/vars-model.js
@@ -0,0 +1,18 @@
+/* 
+ this file models config/vars.js file which should be the one to contain the variables
+*/
+
+let vars = {}
+
+// set database variables
+// replace the variables with your setup variables
+vars.database = {
+    host: 'xxxxxxxxxxxxx',
+    user: 'xxxxxx',
+    password: 'xxxxxxxxxxxxx',
+    database: 'lifeboat' 
+}
+
+
+// export the variables
+module.exports = vars
