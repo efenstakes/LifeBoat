17f87f171346ebb11d8aa4862f99ea8984543dbb
= <efenstakes101@gmail.com>
2019-04-15 13:34:12 +0300

    add more tables to the database file for -foster families, medical histories

--- db.sql
+++ db.sql
@@ -127,7 +127,7 @@ create table supervisor_inspections (
 );
 
 -- keep reports made for kids either by their facility supervisors or government staffers
-create table kids_reports (
+create table foster_kids_reports (
     id int auto_increment,
     foster_kid_id int,
     private_report longtext deafult '',
@@ -137,4 +137,87 @@ create table kids_reports (
     maker_type enum('SUPERVISOR', 'GOV_STAFF'),
     foreign key(foster_kid_id) references foster_kids(id),
     primary key(id)
-)
+);
+
+create table medical_history (
+    id int auto_increment,
+    foster_kid_id int,
+    disease varchar(150),
+    description longtext,
+    ill_on datetime default NOW(),
+    well_on datetime default NOW(),
+    hospital varchar(150),
+    foreign key(foster_kid_id) references foster_kids(id),
+    primary key(id)
+);
+
+
+create table foster_families (
+  id int AUTO_INCREMENT,
+  parent_1_id varchar(40),
+  parent_2_id varchar(40),
+  parent_1_name varchar(40),
+  parent_2_name varchar(40),
+  password varchar(200) not null,
+  parent_1_phone varchar(40),
+  parent_2_phone varchar(40),
+  parent_1_email varchar(40),
+  parent_2_email varchar(40),
+  city varchar(30),
+  verified_by int default null,
+  added_on datetime default NOW(),
+  foreign key(verified_by) references gov_staff(id) on delete set null,
+  primary key(id)
+);
+
+
+
+-- keep history of which homes a foster kid has been to
+-- 
+-- them and before a government staffer oversees it 
+-- so we don't link the column as foreign key to avoid constraint errors
+create table foster_family_kids (
+  id int AUTO_INCREMENT,
+  family_id int,
+  foster_kid_id int,
+  verified_by int default null, 
+  joined_on datetime default NOW(),
+  left_on datetime default NOW(),
+  reason longtext,
+  foreign key(family_id) references foster_families(id) on delete cascade,
+  foreign key(foster_kid_id) references foster_kids(id) on delete cascade,
+  foreign key(verified_by) references gov_staff(id),
+  primary key(id)
+);
+
+
+-- keep history of inspections done in foster family 
+create table family_inspections (
+  id int AUTO_INCREMENT,
+  family_id int,
+  gov_staff_id int,
+  done_on datetime default NOW(),
+  public_report longtext,
+  private_report longtext,
+  status enum('GOOD STANDING', 'STANDING', 'POOR STANDING'),
+  foreign key(family_id) references foster_families(id) on delete cascade,
+  foreign key(gov_staff_id) references gov_staff(id) on delete set null,
+  primary key(id)
+);
+
+
+
+-- table storing requests by families for foster kids 
+create table foster_requests (
+  id int AUTO_INCREMENT,
+  family_id int,
+  foster_kid_id int,
+  gov_staff_id int default null,
+  made_on datetime default NOW(),
+  request_text longtext,
+  status enum('PENDING', 'RECEIVED', 'ACCEPTED', 'REJECTED') default 'PENDING',
+  status_changed datetime default null,  -- when status changed to ACCEPTED or REJECTED 
+  foreign key(family_id) references foster_families(id) on delete cascade,
+  foreign key(gov_staff_id) references gov_staff(id) on delete set null,
+  primary key(id)
+);
